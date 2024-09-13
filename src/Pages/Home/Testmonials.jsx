e9e82d342ef611d0fa1fd8ae730f2431d2c126f6
mehedifiz <mehedi.shanto.contract@gmail.com>
2024-09-13 11:10:29 +0600

    Parallax and Testmonials with react rating

--- src/Pages/Home/Testmonials.jsx
+++ src/Pages/Home/Testmonials.jsx
@@ -1,11 +1,14 @@
 import React, { useEffect, useState } from 'react';
 import Sectiontitle from '../../Comonents/Sectiontitle';
 import { Swiper, SwiperSlide } from 'swiper/react';
+import { Rating } from '@smastrom/react-rating'
+
 
 // Import Swiper styles
 import 'swiper/css';
 import 'swiper/css/navigation';
 import { Navigation } from 'swiper/modules';
+import '@smastrom/react-rating/style.css'
 
 const Testmonials = () => {
 
@@ -26,10 +29,16 @@ const Testmonials = () => {
        {
         reviews.map(review =>  <SwiperSlide
         key={review._id }> 
-        <div className='m-24'>
-            
-            <p>{review.details}</p>
-            <h3 className='text-2xl text-orange-600'>{review.name}</h3>
+        <div className='m-24 flex flex-col items-center mx-24 my-16 gap-2'>
+            <p className='text-7xl text-center'></p>
+        <Rating
+      style={{ maxWidth: 180 }}
+      value={review.rating}
+      readOnly
+    />
+             
+            <p className='px-11 text-center font-Inter  '>{review.details}</p>
+            <h3 className='text-2xl text-orange-600 font-Inter '>{review.name}</h3>
 
 
         </div>
