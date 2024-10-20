2efe6f8b6ef61bc0d8cf877ec23e1ab0880b79e1
mehedifiz <mehedi.shanto.contract@gmail.com>
2024-10-20 22:46:57 +0600

    Setuping stype

--- src/Pages/Deshboard/Payment/Payment.jsx
+++ src/Pages/Deshboard/Payment/Payment.jsx
@@ -1,12 +1,22 @@
+import { loadStripe } from "@stripe/stripe-js";
 import Sectiontitle from "../../../Comonents/Sectiontitle";
+import { Elements } from "@stripe/react-stripe-js";
+import CheckOutfrom from "./CheckOutfrom";
+
+// toto : add pk 
+const stripePromise = loadStripe(import.meta.env.VITE_PEYMENT)
 
 const Payment = () => {
+
     return (
-        <div>
+        <div className="h-[100vh]">
 
             <Sectiontitle heading='Payment' subheading='Pay to eat'></Sectiontitle>
 
             <div>
+                <Elements stripe={stripePromise}>
+                    <CheckOutfrom/>
+                </Elements>
                 
             </div>
             
