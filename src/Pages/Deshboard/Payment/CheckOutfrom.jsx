c78fcbda16b59b79041c1c9816516f7b3da2d9f5
mehedifiz <mehedi.shanto.contract@gmail.com>
2024-10-22 21:23:47 +0600

    payment method learning

--- src/Pages/Deshboard/Payment/CheckOutfrom.jsx
+++ src/Pages/Deshboard/Payment/CheckOutfrom.jsx
@@ -1,8 +1,39 @@
 import { CardElement, useElements, useStripe } from "@stripe/react-stripe-js";
+import { useEffect, useState } from "react";
+import useAxios from "../../../Hooks/useAxios";
+import useCart from "../../../Hooks/useCart";
+import useAuth from "../../../Hooks/useAuth";
+import { toast } from "react-toastify";
 
 const CheckOutfrom = () => {
+
+  const [error , setError] = useState('')
     const stripe = useStripe();
     const elements = useElements();
+  const axiosSecure = useAxios();
+  const {user} = useAuth();
+  const [tarID , setTraID] = useState('')
+
+  const [cart] = useCart();
+
+  const[clientSecret , setClientSecret] = useState('')
+
+  const price = cart.reduce((total , item ) => total + item.price ,0)
+
+
+    useEffect(()=>{
+
+
+      axiosSecure.post('/create-payment-intrent', {price})
+      .then(res=>{
+        console.log(res.data.clientSecret);
+        setClientSecret(res.data.clientSecret)
+      })
+
+    }
+
+
+    ,[axiosSecure , price])
     const handleSubmit = async (event)=>{
 
             event.preventDefault();
@@ -23,7 +54,38 @@ const CheckOutfrom = () => {
 
             if(error){
                 console.log(error)
-            } else{console.log(paymentMethod)}
+                setError(error.message)
+            } else{
+              setError('')
+              console.log('payment mathod ',paymentMethod)
+            
+            }
+
+
+            //confirm payment 
+
+            const {paymentIntent , error: confirmError} = await stripe.confirmCardPayment(clientSecret ,{
+              payment_method:{
+                card: card,
+                billing_details:{
+                  email: user?.email,
+                  name: user?.name
+
+                }
+              }
+            })
+            if(confirmError){
+              console.log('comfirm error')
+            } 
+            else{
+              console.log('payment intrent' , paymentIntent)
+            }
+
+
+            if(paymentIntent.status === 'succeeded'){
+              toast.success(`Payment succeeded` ,paymentIntent.id )
+              setTraID(paymentIntent.id)
+            }
 
             
             
@@ -48,9 +110,12 @@ const CheckOutfrom = () => {
           },
         }}
       />
-      <button className="btn btn-sm btn-primary" type="submit" disabled={!stripe}>
+      <button  className="btn btn-sm btn-primary" type="submit" disabled={!stripe || !clientSecret }>
         Pay
       </button>
+      <p className="text-red-600">{error}</p>
+       {tarID && <p className="text-green-500">Your Transaction id: {tarID}</p>
+      }
        </form>
     );
 };
