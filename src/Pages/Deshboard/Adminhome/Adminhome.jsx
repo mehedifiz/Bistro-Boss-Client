b411e97ebfdfdbb1dda64946583f1eb63ba399e2
mehedifiz <mehedi.shanto.contract@gmail.com>
2024-10-25 18:54:40 +0600

    rechart in admin home

--- src/Pages/Deshboard/Adminhome/Adminhome.jsx
+++ src/Pages/Deshboard/Adminhome/Adminhome.jsx
@@ -1,101 +1,128 @@
+import React from "react";
 import { useQuery } from "@tanstack/react-query";
 import useAuth from "../../../Hooks/useAuth";
 import useAxios from "../../../Hooks/useAxios";
 import { FaDollarSign } from "react-icons/fa";
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, PieChart, Pie, ResponsiveContainer, Legend } from "recharts";
 import Loading from "../../../Comonents/Loading";
 
 const Adminhome = () => {
-    const {user} = useAuth();
-    const axiosSecure = useAxios()
+  const { user } = useAuth();
+  const axiosSecure = useAxios();
+  const colors = ["#0088FE", "#00C49F", "#FFBB28", "#FF8042", "red", "pink"];
+  const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+  const RADIAN = Math.PI / 180;
 
-    const {data : stat , isLoading} = useQuery({
-        queryKey:['admin-stats'],
+  const { data: stat, isLoading: statLoading } = useQuery({
+    queryKey: ["admin-stats"],
+    queryFn: async () => {
+      const res = await axiosSecure.get("/admin-stats");
+      return res.data;
+    },
+  });
 
-        queryFn: async()=>{
-            const res =await axiosSecure.get('/admin-stats');
-            return res.data;
-        }
-    })
+  const { data: orderData = [], isLoading: orderLoading } = useQuery({
+    queryKey: ["order-stat"],
+    queryFn: async () => {
+      const res = await axiosSecure("/order-stats");
+      return res.data;
+    },
+  });
 
-    if (isLoading) {
-        return <Loading />;      }
-    
+  if (statLoading || orderLoading) {
+    return <Loading></Loading>
+  }
+
+  const PiechartData = orderData.map(data => ({
+    name: data.category,
+    value: data.revenue
+  }));
+
+  const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+    const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
+    const x = cx + radius * Math.cos(-midAngle * RADIAN);
+    const y = cy + radius * Math.sin(-midAngle * RADIAN);
+
     return (
-        <div>
-
-             <h2 className="text-3xl">Hi , Welcome {user?.displayName ? user.displayName : 'Back'} </h2> 
-
-
-
-             <div className="stats shadow">
-  <div className="stat">
-    <div className="stat-figure text-secondary">
-    {/* <FaDollarSign className="text-4xl"/> */}
-    </div>
-    <div className="stat-title">Revenue</div>
-    <div className="stat-value">${stat.revenue}</div>
-    <div className="stat-desc">Jan 1st - Feb 1st</div>
-  </div>
-
-  <div className="stat">
-    <div className="stat-figure text-secondary">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        className="inline-block h-8 w-8 stroke-current">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M12 6V4m0 2a2 2 0 100 4m0-4a2 2 0 110 4m-6 8a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4m6 6v10m6-2a2 2 0 100-4m0 4a2 2 0 110-4m0 4v2m0-6V4"></path>
-      </svg>
-    </div>
-    <div className="stat-title"> Users</div>
-    <div className="stat-value">{stat.users}</div>
-    <div className="stat-desc">↗︎ 400 (22%)</div>
-  </div>
-
-  <div className="stat">
-    <div className="stat-figure text-secondary">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        className="inline-block h-8 w-8 stroke-current">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path>
-      </svg>
-    </div>
-    <div className="stat-title">Orders</div>
-    <div className="stat-value">{stat.orders}</div>
-    <div className="stat-desc">↘︎ 90 (14%)</div>
-  </div>
-
-  <div className="stat">
-    <div className="stat-figure text-secondary">
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        className="inline-block h-8 w-8 stroke-current">
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          strokeWidth="2"
-          d="M5 8h14M5 8a2 2 0 110-4h14a2 2 0 110 4M5 8v10a2 2 0 002 2h10a2 2 0 002-2V8m-9 4h4"></path>
-      </svg>
-    </div>
-    <div className="stat-title">Menu Items</div>
-    <div className="stat-value">{stat.menuItems}</div>
-    <div className="stat-desc">↘︎ 90 (14%)</div>
-  </div>
-</div>
-        </div>
+      <text x={x} y={y} fill="white" textAnchor={x > cx ? "start" : "end"} dominantBaseline="central">
+        {`${(percent * 100).toFixed(0)}%`}
+      </text>
     );
+  };
+
+  return (
+    <div>
+      <h2 className="text-3xl mb-6">Hi, Welcome {user?.displayName || "Back"}</h2>
+
+      <div className="stats shadow mb-8">
+        <div className="stat">
+          <div className="stat-figure text-secondary">
+            <FaDollarSign className="text-4xl" />
+          </div>
+          <div className="stat-title">Revenue</div>
+          <div className="stat-value">${stat.revenue}</div>
+          <div className="stat-desc">Jan 1st - Feb 1st</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-title">Users</div>
+          <div className="stat-value">{stat.users}</div>
+          <div className="stat-desc">↗︎ 400 (22%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-title">Orders</div>
+          <div className="stat-value">{stat.orders}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+
+        <div className="stat">
+          <div className="stat-title">Menu Items</div>
+          <div className="stat-value">{stat.menuItems}</div>
+          <div className="stat-desc">↘︎ 90 (14%)</div>
+        </div>
+      </div>
+
+      <div className="flex  ">
+        <div className="w-full lg:w-1/2">
+          <ResponsiveContainer width="100%" height={300}>
+            <BarChart data={orderData}>
+              <CartesianGrid strokeDasharray="3 3" />
+              <XAxis dataKey="category" />
+              <YAxis />
+              <Bar dataKey="quantity" fill="#8884d8" label={{ position: 'top' }}>
+                {orderData.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={colors[index % colors.length]} />
+                ))}
+              </Bar>
+            </BarChart>
+          </ResponsiveContainer>
+        </div>
+
+        <div className="w-full lg:w-1/2">
+          <ResponsiveContainer width="100%" height={300}>
+            <PieChart>
+              <Pie
+                data={PiechartData}
+                cx="50%"
+                cy="50%"
+                labelLine={false}
+                label={renderCustomizedLabel}
+                outerRadius={80}
+                fill="#8884d8"
+                dataKey="value"
+              >
+                {PiechartData.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+              </Pie>
+              <Legend/>
+            </PieChart>
+          </ResponsiveContainer>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Adminhome;
